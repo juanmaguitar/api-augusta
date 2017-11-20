c51cd7f20d942b7fd0bbc480fd5a10b0fbead6a5
Abel <canet.ach@gmail.com>
2017-11-20 13:21:24 +0100

    solucionar problema buscqueda

--- server/routes/private/handlers/getCustomersPayments.js
+++ server/routes/private/handlers/getCustomersPayments.js
@@ -1,17 +1,68 @@
 const Customers = require('../../../models/Customers')
 const Articles = require('../../../models/Articles')
-function getCustomers( req, res ) {
+const ObjectId = require('mongoose').Types.ObjectId; 
+function getCustomersPayments( req, res ) {
+//  Articles.find({'state':'finalized'}, function(err, article) {
+//       Customers.populate(article, {path: "customer_id"},function(err, article){
+//           const result ={
+//             "contact": "abel",
+//             "avg": 450
+//           }
+//           const resultado= article.reduce(function(valorAnterior, valorActual,i, article){
+//             if(valorActual.customer_id._id === valorActual.customer_id.contact){
+//               return valorActual.customer_id
+//             }
+//             //return valorActual.customer_id.contact
+//             //if(article.customer_id._id){}
+//           },{})
+//           res.json(resultado);
+//         })
+//     })  
+// }
+
   // Customers.find({}, function(err, customers) {
-  //   console.log(customers[0]._id)
-  //     Articles.findById(customers[0]._id,function(err,article){
-  //       res.json(article);
-  //     })  
-  // })
-  Articles.findOne({'customer_id':'5a0c0ac463bb9ae2b6d99323'},function(err,article){
-    res.json(article);
-  })  
+  //   Articles.find({'customer_id':ObjectId('5a0c0ac463bb9ae2b6d99323')},function(err,articles){
+  //     res.json(articles); 
+  //   })
+  //   //res.json(customers);  
+  // });
+
+  // Customers.find()
+  //   .then( customers => {
+  //     return Articles.find({ "customer_id": customers[0]._id })
+  //   })
+  //   .then( articles => {res.json(articles)})
+    
+
+  Customers.findById('5a0c0ac463bb9ae2b6d99323')
+    .then( customer => {
+      console.log(customer._id)
+      return Articles.find({ "customer_id": customer._id }) 
+    })
+    .then( articles => {
+      res.json(articles)
+    })  
+  // Customers.findById("5a0c0ac463bb9ae2b6d99323")
+  //   .then( customer => {
+  //     console.log(customer)
+  //     Articles.find({ "customer_id": customer._id }) 
+  //   })
+  //   .then( articles => {
+  //     res.json(articles)
+  //   })
+  //   .catch( error =>{
+  //     res.json({ error })
+  //   })
+
+    // ))
+    // .then( articles => {res.json(articles)})
+  
 }
-//5a0c0ac463bb9ae2b6d99323
-module.exports = getCustomers
+module.exports = getCustomersPayments
 
+//Cliente | avg | Pagado | Diferencia
 
+// const result ={
+//             "contact": "abel",
+//             "avg": 450
+//           }
