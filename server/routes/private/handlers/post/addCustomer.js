74be0d9caf14a5ac9fb5a8dc7b05fe860b7e9a8f
Abel <canet.ach@gmail.com>
2017-11-22 23:57:38 +0100

    endpoint post customer funcionando

--- server/routes/private/handlers/post/addCustomer.js
+++ server/routes/private/handlers/post/addCustomer.js
@@ -1,11 +1,13 @@
 const Customers = require('../../../../models/Customers')
 
 function addCustomer( req, res ) {
+
   const newCustomer = new Customers(req.body);
   newCustomer.save(function (err) {
   if (err) return handleError(err);
+   // saved!
   res.status(200).send("Guardado");
-});
+})
 }
 
 module.exports = addCustomer
