dd90087d78e9060f1a296df3d05834e45e9b0d01
Abel <canet.ach@gmail.com>
2017-11-22 15:07:23 +0100

    si no se pasa id al endpoint de customer ahor ya no peta

--- server/routes/private/handlers/update/updateCustomer.js
+++ server/routes/private/handlers/update/updateCustomer.js
@@ -1,11 +1,15 @@
 const Customers = require('../../../../models/Customers')
 
 function updateCustomer( req, res ) {
-  const {id,body} =req.params 
-  Customers.findByIdAndUpdate(id, { $set: req.body}, { new: true }, function (err, customer) {
-    if (err) return handleError(err);
-    res.send(customer);
-  });
+  const {id,body} =req.params
+  if(id === 'undefined'){
+    res.status(400).send('no pass ID customer');
+  }else {
+    Customers.findByIdAndUpdate(id, { $set: req.body}, { new: true }, function (err, customer) {
+      if (err) return handleError(err);
+      res.send(customer);
+    })
+  }
 }
 
 module.exports = updateCustomer
