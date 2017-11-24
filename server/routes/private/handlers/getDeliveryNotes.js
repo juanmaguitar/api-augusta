f59ec9ca956d8ab2bf6cc5d58e8c1048562eb518
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-11-24 13:08:27 +0100

    some comment

--- server/routes/private/handlers/getDeliveryNotes.js
+++ server/routes/private/handlers/getDeliveryNotes.js
@@ -1,12 +1,11 @@
 const DeliveryNotes = require('../../../models/DeliveryNotes')
 const Customers = require('../../../models/Customers')
 
+/* this function fet delivery notes... */
 async function getDeliveryNotes( req, res ) {
-
 	const notes = await DeliveryNotes.find()
 	const deliveryNote = await Customers.populate(notes, { path: "customer_id" })
 	res.status(200).json(deliveryNote);
-
 }
 
 module.exports = getDeliveryNotes
