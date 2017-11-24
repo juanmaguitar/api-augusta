acb26d0c55d04c01fa0bd8f60a2b9b21d8daa094
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-11-24 12:43:54 +0100

    async await deliveryNotes

--- server/routes/private/handlers/getDeliveryNotes.js
+++ server/routes/private/handlers/getDeliveryNotes.js
@@ -1,12 +1,21 @@
 const DeliveryNotes = require('../../../models/DeliveryNotes')
 const Customers = require('../../../models/Customers')
 
-function getDeliveryNotes( req, res ) {
- 	DeliveryNotes.find({}, function(err, deliveryNote) {
-    	Customers.populate(deliveryNote, {path: "customer_id"},function(err, deliveryNote){
-        	res.status(200).json(deliveryNote);
-        })
-    })	
+async function getDeliveryNotes( req, res ) {
+
+	const notes = await DeliveryNotes.find()
+	const deliveryNote = await Customers.populate(notes, { path: "customer_id" })
+	res.status(200).json(deliveryNote);
+
+	// DeliveryNotes.find()
+	// 	.then(notes => Customers.populate(notes, {path: "customer_id"}) )
+	// 	.then(deliveryNote => res.status(200).json(deliveryNote) )
+	
+ 	// DeliveryNotes.find({}, function(err, deliveryNote) {
+    // 	Customers.populate(deliveryNote, {path: "customer_id"},function(err, deliveryNote){
+    //     	res.status(200).json(deliveryNote);
+    //     })
+    // })	
 }
 
 module.exports = getDeliveryNotes
