44369e9aa48d2b860c5924a892bb0199d8b258fc
Abel <canet.ach@gmail.com>
2017-11-24 13:03:52 +0100

    refactorizar

--- server/routes/private/handlers/getDeliveryNotes.js
+++ server/routes/private/handlers/getDeliveryNotes.js
@@ -6,7 +6,7 @@ async function getDeliveryNotes( req, res ) {
 	const notes = await DeliveryNotes.find()
 	const deliveryNote = await Customers.populate(notes, { path: "customer_id" })
 	res.status(200).json(deliveryNote);
-
+  //el status 200 no sería necesario pero aclara el código
 }
 
 module.exports = getDeliveryNotes
